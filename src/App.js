862e225277004d3ff8c5359ef0e1a695ace9cd2a
SGrossett <selene_816@hotmail.com>
2022-05-11 23:38:51 -0400

    Create a component for the about page and render it in the app.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import AboutPage from './components/About';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
+      <AboutPage />
     </div>
   );
 }
