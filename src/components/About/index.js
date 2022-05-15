f936b6b848a5bd1211c7b1a927d18d21e0a64f0b
SGrossett <selene_816@hotmail.com>
2022-05-14 21:04:04 -0400

    Make the get request dependent on the tweets so new tweets are renders to to the app on submit.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,20 @@
+import { Row, Col, Container } from 'react-bootstrap';
 function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <div>
+      <Container>
+        <Row>
+          <p>Hello, my name is</p>
+        </Row>
+        <Row>
+          <h2>Selene Grossett</h2>
+        </Row>
+        <Row>
+          <h2>Programmer, Designer and Book Lover</h2>
+          {true}
+        </Row>
+      </Container>
+    </div>
   )
 }
 
