136e4cb97302ed9681372152e3d5c91afbbbc5fb
SGrossett <selene_816@hotmail.com>
2022-06-03 15:28:54 -0400

    Create a route for the contact page and link the hire button on the home page to it.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/Home';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-            
-          <Route path='/projects' element={<Projects />} />
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </Router>
   );
 }
