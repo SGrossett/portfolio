f9ddf2244d8ff04410c4ceaa3c4cb62c204962af
SGrossett <selene_816@hotmail.com>
2022-06-05 17:45:39 -0400

    Add spacing to the contact labels and form inputs. Make the labels uppercase.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import './contact.css';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 function Contact() {
   return (
-    <Container className="bg-white">
-      <Row className='bg-success mx-5 pt-5 pb-5'>
-        <Col className='bg-primary mx-'>
-          <h1>Contact</h1>
+    <Container className="contact-bg">
+      <Row className='mx-4 pt-5 pb-5'>
+        <Col className='bg-primary px-3' lg={5} >
+          <h1 className='pt-3'>Contact</h1>
           <p> Not a fan of forms? Still get in touch and shoot me an email &nbsp;
             <em>
               <a 
@@ -17,23 +17,23 @@ function Contact() {
             .
           </p>
         </Col>
-        <Col xs={1}></Col>
-        <Col className='bg-secondary'>
-          <Form>
-            <Form.Group className="mb-3" controlId="formName">
-              <Form.Label>Full name</Form.Label>
+        <Col ></Col>
+        <Col className='bg-secondary' lg={6}>
+          <Form className='pb-3'>
+            <Form.Group className="mb-3 pt-3" controlId="formName">
+              <Form.Label className='text-uppercase'>Full name</Form.Label>
               <Form.Control type="text" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formEmail">
-              <Form.Label>Email</Form.Label>
+              <Form.Label className='text-uppercase'>Email</Form.Label>
               <Form.Control type="email" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="fromMessage">
-              <Form.Label>Message</Form.Label>
+            <Form.Group className="mb-4" controlId="fromMessage">
+              <Form.Label className='text-uppercase'>Message</Form.Label>
               <Form.Control as="textarea" rows={5} />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            <Button variant="primary mb-2" type="submit">
               Send Message
             </Button>
           </Form>
