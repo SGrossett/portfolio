127ec0a44014dd3e552021e5ffadd9ab77b72646
SGrossett <selene_816@hotmail.com>
2022-06-05 16:08:54 -0400

    Add markark for the form using react bootstrap form component.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col, Form } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 function Contact() {
   return (
@@ -7,7 +7,26 @@ function Contact() {
       <Row className='bg-success mx-5 pt-5 pb-5'>
         <Col className='bg-primary mx-'>1</Col>
         <Col xs={1}></Col>
-        <Col className='bg-secondary'>2</Col>
+        <Col className='bg-secondary'>
+          <Form>
+            <Form.Group className="mb-3" controlId="formName">
+              <Form.Label>Full name</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="fromMessage">
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textarea" rows={5} />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Send Message
+            </Button>
+          </Form>
+        </Col>
       </Row>
     </Container>
   )
