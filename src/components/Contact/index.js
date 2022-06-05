8daff7cd63f508c38037535be1608700e1df0f4e
SGrossett <selene_816@hotmail.com>
2022-06-05 11:39:16 -0400

    Create a grid template for the contact form.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Container, Row, Col, Form } from 'react-bootstrap';
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <Container className="bg-white">
+      <Row className='bg-success mx-5 pt-5 pb-5'>
+        <Col className='bg-primary mx-'>1</Col>
+        <Col xs={1}></Col>
+        <Col className='bg-secondary'>2</Col>
+      </Row>
+    </Container>
   )
 }
 
