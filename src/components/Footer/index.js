b391e3b4c086fad2867fdd9e85e666f17c7eb296
SGrossett <selene_816@hotmail.com>
2022-06-05 01:44:57 -0400

    Increase the font size on my name and add a margin to the copyright so it lines up with the year.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,21 @@
 import './footer.css';
 import React from 'react'
 import { Container, Nav, Col, Row } from 'react-bootstrap';
-import { RiMoonFill } from 'react-icons/ri';
+import { RiMoonFill,RiCopyrightLine } from 'react-icons/ri';
 import { FaLinkedinIn } from 'react-icons/fa';
 import { BsGithub, BsMedium } from 'react-icons/bs';
+import { AiOutlineCopyrightCircle } from 'react-icons/ai';
 
 function Footer() {
   return (
     <Container className='d-flex footer-border mt-5' fluid>
-      <Col className='d-flex mt-2'>
+      <Col className='d-flex mt-3'>
         <p className='mb-0 nav-padding text-white'>
           <span className='d-block pb-1 text-neonPink'>Built by: &nbsp;</span>
-          <span className=''>Selene <RiMoonFill className='crescentMoon'/> Grossett</span>
+          <span className='my-name'>Selene <RiMoonFill className='crescentMoon'/> Grossett</span>
         </p>
       </Col>
-      <Col className='mt-2 mb-1'>
+      <Col className='mt-2 mb-2'>
         <Nav className='justify-content-end'>
           <Nav.Link href="https://github.com/SGrossett" target='_blank' className='text-neonPink p-2 pb-1'>
             <BsGithub size={22} />
@@ -26,7 +27,7 @@ function Footer() {
             <BsMedium size={22} />
           </Nav.Link>
         </Nav>
-        <p className='d-flex justify-content-end text-white'>2022</p>
+        <p className='d-flex justify-content-end text-white'><AiOutlineCopyrightCircle size={18} className='copyright'/> &nbsp; 2022</p>
       </Col>
     </Container>
   )
