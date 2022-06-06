0841d5db0934d8efcc662035f96055ecd42a7858
SGrossett <selene_816@hotmail.com>
2022-06-06 13:25:17 -0400

    Add arrow to laptop girl.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,13 @@
 import './contact.css';
 import { Container, Row, Col, Form, Button, Stack, Image } from 'react-bootstrap';
-import laptopGirl from './images/laptopGirl.png';
+import laptopGirl from './images/laptopGirl-arrow.png';
 
 function Contact() {
   return (
-    <Container className="contact-bg rounded-container">
-      <Row className='mx-4 pt-5 pb-5'>
-        <Col className='bg-primary px-3 ' lg={5} >
-          <h1 className='pt-3 contact-h1'>Contact</h1>
+    <Container className="">
+      <Row className='mx-4 '>
+        <Col className='b-primary px-3 b-image' lg={5} >
+          <h1 className='pt-3 contact-h1 text-info'>Contact</h1>
           <p> Not a fan of forms? Still get in touch and shoot me an email &nbsp;
             <em>
               <a 
@@ -17,24 +17,25 @@ function Contact() {
             </em>
             .
           </p>
-          <Col className='d-flex'>
-            <Image src={laptopGirl} />
+          <Col className='d-flex justify-content-center mt-5'>
+            <Image src={laptopGirl}/>
           </Col>
         </Col>
         <Col ></Col>
-        <Col className='bg-secondary' lg={6}>
+        <Col className='b-secondary p-4 contact-bg rounded-containerlaptopGirl-
+        ' lg={6}>
           <Form className='pb-3'>
             <Form.Group className="mb-3 pt-3" controlId="formName">
-              <Form.Label className='text-uppercase'>Full name</Form.Label>
+              <Form.Label className='text-uppercase text-info'>Full name</Form.Label>
               <Form.Control type="text" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formEmail">
-              <Form.Label className='text-uppercase'>Email</Form.Label>
+              <Form.Label className='text-uppercase text-info'>Email</Form.Label>
               <Form.Control type="email" />
             </Form.Group>
             <Form.Group className="mb-4" controlId="fromMessage">
-              <Form.Label className='text-uppercase'>Message</Form.Label>
+              <Form.Label className='text-uppercase text-info'>Message</Form.Label>
               <Form.Control as="textarea" rows={8} />
             </Form.Group>
             <Button variant="primary mb-2" type="submit">
