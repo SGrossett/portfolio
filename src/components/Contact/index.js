5aa0033588443e9ac8abfe3182f556bb375d121f
SGrossett <selene_816@hotmail.com>
2022-06-06 13:52:15 -0400

    Change the handle on the contact page. Update the font colours on the form labels and handle so they are easier to read.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,11 +8,11 @@ function Contact() {
       <Row className='mx-4 '>
         <Col className='b-primary px-3' lg={5} >
           <h1 className='pb-1 contact-h1 text-info'>Contact</h1>
-          <p> Not a fan of forms? Still get in touch and shoot me an email &nbsp;
+          <p className='text-info2'>Get in touch or email me directly &nbsp;
             <em>
               <a 
                 href='mailto:selene.grossett@gmail.com'
-                className='text-black font-bold'
+                className='text-white font-bold'
               >here</a>
             </em>
             .
@@ -25,16 +25,16 @@ function Contact() {
         <Col className='b-secondary p-4 contact-bg rounded-container' lg={6}>
           <Form className='pb-3 px-3'>
             <Form.Group className="mb-3 pt-3" controlId="formName">
-              <Form.Label className='text-uppercase text-info'>Full name</Form.Label>
+              <Form.Label className='text-uppercase text-bg'>Full name</Form.Label>
               <Form.Control type="text" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formEmail">
-              <Form.Label className='text-uppercase text-info'>Email</Form.Label>
+              <Form.Label className='text-uppercase text-bg'>Email</Form.Label>
               <Form.Control type="email" />
             </Form.Group>
             <Form.Group className="mb-4" controlId="fromMessage">
-              <Form.Label className='text-uppercase text-info'>Message</Form.Label>
+              <Form.Label className='text-uppercase text-bg'>Message</Form.Label>
               <Form.Control as="textarea" rows={8} />
             </Form.Group>
             <Button variant="primary " type="submit">
