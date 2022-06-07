ba3400c6f5f40d212ba082b516e204913e5f7ea7
SGrossett <selene_816@hotmail.com>
2022-06-07 15:55:24 -0400

    Add a title and cta to the resume page.

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Resume() {
   return (
-    <div>Resume</div>
+    <Container className="contact-container">
+      <Row className='mx-4'>
+        <Col className='px-3'>
+          <h1 className='pb-1 contact-h1 text-info'>Resume</h1>
+          <p className='text-info2'>Reach out on my contact page or click to &nbsp; 
+            <em>
+              <a 
+                href='mailto:selene.grossett@gmail.com'
+                className='text-white font-bold'
+              >download</a>
+            </em>
+            &nbsp; my resume.
+          </p>
+        </Col>
+      </Row>
+
+    </Container> 
   )
 }
 
